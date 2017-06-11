df323d3763ba1229b729f9370f6b020503bb36e9
hidder11 <hidder11@gmail.com>
2017-06-11 23:55:32 +0200

    Homepage Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+    <div class="row m-0">
+        <div class="col-md-3 card home-block px-0"><!--Card image-->
+            <div class="view overlay hm-white-slight home-block-image">
+                <img src="https://cookinglife.nl/uploads/webshop/hertog%202.jpg" class="" alt="">
+                <a>
+                    <div class="mask waves-effect waves-light"></div>
+                </a>
             </div>
+            <!--/.Card image-->
+
+            <!--Button-->
+            <a class="btn-floating btn-action"><i class="fa fa-chevron-right"></i></a>
+
+            <!--Card content-->
+            <div class="card-block">
+                <!--Title-->
+                <h4 class="card-title">Card title</h4>
+                <hr>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+            <!--/.Card content-->
+        </div>
+        <div class="col-md-3 card home-block px-0"><!--Card image-->
+            <div class="view overlay hm-white-slight home-block-image">
+                <img src="https://cookinglife.nl/uploads/webshop/hertog%202.jpg" class="" alt="">
+                <a>
+                    <div class="mask waves-effect waves-light"></div>
+                </a>
+            </div>
+            <!--/.Card image-->
+
+            <!--Button-->
+            <a class="btn-floating btn-action"><i class="fa fa-chevron-right"></i></a>
+
+            <!--Card content-->
+            <div class="card-block">
+                <!--Title-->
+                <h4 class="card-title">Card title</h4>
+                <hr>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+            <!--/.Card content-->
+            >
+        </div>
+        <div class="col-md-3 card home-block px-0"><!--Card image-->
+            <div class="view overlay hm-white-slight home-block-image">
+                <img src="https://cookinglife.nl/uploads/webshop/hertog%202.jpg" class="" alt="">
+                <a>
+                    <div class="mask waves-effect waves-light"></div>
+                </a>
+            </div>
+            <!--/.Card image-->
+
+            <!--Button-->
+            <a class="btn-floating btn-action"><i class="fa fa-chevron-right"></i></a>
+
+            <!--Card content-->
+            <div class="card-block">
+                <!--Title-->
+                <h4 class="card-title">Card title</h4>
+                <hr>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+            <!--/.Card content-->
+        </div>
+        <div class="col-md-3 card home-block px-0 fb-container">
+            {{--<div class="fb-page card-block p-0"--}}
+            {{--data-href="https://www.facebook.com/JansmaHaule.nl"--}}
+            {{--data-tabs="timeline"--}}
+            {{--data-width="500"--}}
+            {{--data-height="500"--}}
+            {{--data-hide-cover="false"--}}
+            {{--data-show-facepile="false"--}}
+            {{--data-show-posts="false">--}}
+
+            {{--</div>--}}
         </div>
     </div>
-</div>
+    </div>
 @endsection
